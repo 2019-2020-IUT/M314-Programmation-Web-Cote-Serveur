a001aab11f4b7aa989fc6bbd21e2b0847cada258
JunkJumper <jose.srifi@gmail.com>
2020-09-14 11:14:18 +0200

    "debut TD2"

---
+++ 2020-2021/TD2/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<?php
+/**
+ * @Author: JunkJumper
+ * @Link: https://github.com/JunkJumper
+ * @Copyright: Creative Common 4.0 (CC BY 4.0)
+ * @Create Time: 07-09-2020 10:44:25
+ * @Description: M314 - TD1
+ */
+?>
+
+<html>
+
+<head>
+    <title>M314 - TD1</title>
+    <link rel="icon" type="image/png" href="https://www.junkjumper-projects.com/umming_lines_paint.png" />
+</head>
+
+<body>
+    <a href="..">
+        <button class="button blue">Retour à la page précédente</button>
+    </a>
+</body>
+
+<style>
+    .button {
+        border: none;
+        color: white;
+        padding: 15px 32px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;
+        margin: 4px 2px;
+        cursor: pointer;
+    }
+
+    .blue {
+        background-color: #4c6e8a;
+    }
+
+    /* Green */
+</style>
+
+
+</html>
